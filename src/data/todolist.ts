728d30fa9bdbc572682e7dfd1589ff621e051cf1
steve <steve57b@hotmail.fr>
2025-03-13 16:58:53 +0100

    ajout d'un fichier data

---
+++ src/data/todolist.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    "id": 1,
+    "description": "Courir avec le fraté",
+    "done" : false
+  },
+  {
+    "id": 2,
+    "description": "Me faire défoncer à LoL",
+    "done" : true
+  },
+  {
+    "id": 3,
+    "description": "Manger avec Laurent",
+    "done" : false
+  }
+];
