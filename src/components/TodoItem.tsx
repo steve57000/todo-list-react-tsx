e66ef07b73714688c90784a2016cddb82af8da6c
steve <steve57b@hotmail.fr>
2025-03-14 20:59:51 +0100

    ajustement de la couleur de fond des listes

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,5 @@
 import {BtnRemove} from "./BtnRemove.tsx";
+import styled from 'styled-components';
 
 interface TodoItemProps {
   id: number;
@@ -8,13 +9,18 @@ interface TodoItemProps {
   onRemove: (id: number) => void;
 }
 
+const BoxItem = styled.div`
+  box-shadow: 2px 3px 5px rgba(0, 0, 0, 0.4);
+`
+
+
 export function TodoItem({ id, description, done, onToggle, onRemove }: TodoItemProps) {
   const containerClass = done
     ? "bg-indigo-900 w-full rounded-box p-3 flex items-center"
     : "bg-indigo-700 w-full rounded-box p-3 flex items-center";
 
   return (
-    <div className={containerClass}>
+    <BoxItem className={containerClass}>
       <span className="pr-4">
         <input
           type="checkbox"
@@ -25,7 +31,7 @@ export function TodoItem({ id, description, done, onToggle, onRemove }: TodoItem
 
       <span className="flex-grow">{description}</span>
 
-      <BtnRemove onClick={() => onRemove(id)} />
-    </div>
+      <BtnRemove onClick={() => onRemove(id)} contentTxt="X" addClass="btn btn-error btn-outline btn-xs ml-4" />
+    </BoxItem>
   );
 }
--- src/components/TodoListCard.tsx
+++ src/components/TodoListCard.tsx
@@ -20,21 +20,24 @@ export function TodoListCard({
   // État local pour afficher/masquer le contenu de la liste (accordéon)
   const [isOpen, setIsOpen] = useState(false);
 
+  const colorBackgroundIsOpen = isOpen ? "bg-indigo-400 border rounded shadow-md p-4" : "bg-indigo-700 border rounded shadow-md p-4";
+  const colorTextIsOpen = isOpen ? "text-black text-lg font-bold" : "text-lg font-bold";
+
   // Tri local : on affiche d'abord les items non faits, puis les faits
   const sortedItems = [...list.items].sort(
     (a, b) => Number(a.done) - Number(b.done)
   );
 
   return (
-    <div className="grid grid-cols-[3fr_40px] items-center">
-      <div className="border rounded shadow-md p-4">
+    <div className="grid grid-cols-[3fr_150px] items-center">
+      <div className={colorBackgroundIsOpen}>
         {/* En-tête de la carte : titre cliquable qui toggle l'ouverture */}
         <div
           className="flex justify-between items-center cursor-pointer"
           onClick={() => setIsOpen(!isOpen)}
         >
-          <h2 className="text-lg font-bold">{list.title}</h2>
-          <span>{isOpen ? "▼" : "►"}</span>
+          <h2 className={colorTextIsOpen}>{list.title}</h2>
+          <span>{isOpen ? "ᐃ" : "ᐁ"}</span>
         </div>
         {/* Contenu de la liste, visible seulement si isOpen = true */}
         {isOpen && (
@@ -53,7 +56,7 @@ export function TodoListCard({
         )}
       </div>
 
-      <BtnRemove onClick={() => onRemoveList(list.id)} />
+      <BtnRemove onClick={() => onRemoveList(list.id)} contentTxt={`supprimer ${list.title}`}  addClass="bg-indigo-700 w-150 rounded-box p-3 flex items-center" />
   </div>
   );
 }
