73b64ad1cc61210dfd99a43526684facca4d4af1
steve <steve57b@hotmail.fr>
2025-03-18 14:04:58 +0100

    ajout de la classe qui permet de barré de la liste un item

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -19,6 +19,10 @@ export function TodoItem({ id, description, done, onToggle, onRemove }: TodoItem
     ? "bg-indigo-900 w-full rounded-box p-3 flex items-center"
     : "bg-indigo-700 w-full rounded-box p-3 flex items-center";
 
+  const textLineThrough = done
+    ? "flex-grow line-through"
+    : "flex-grow"
+
   return (
     <BoxItem className={containerClass}>
       <span className="pr-4">
@@ -29,7 +33,7 @@ export function TodoItem({ id, description, done, onToggle, onRemove }: TodoItem
         />
       </span>
 
-      <span className="flex-grow">{description}</span>
+      <span className={textLineThrough}>{description}</span>
 
       <BtnRemove onClick={() => onRemove(id)} contentTxt="X" addClass="btn btn-error btn-outline btn-xs ml-4" />
     </BoxItem>
